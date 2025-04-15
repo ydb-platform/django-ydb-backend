53c49ae58c43bf46b888e3cd5f49b3568b6d29f9
MirzoFirdavs <mirzofirdavs@github.com>
2025-04-15 17:04:42 +0300

    add method

--- ydb_backend/backend/operations.py
+++ ydb_backend/backend/operations.py
@@ -420,3 +420,8 @@ class DatabaseOperations(BaseDatabaseOperations):
 
     def adapt_json_value(self, value, encoder):
         return json.load(value)
+
+    def bulk_insert_sql(self, fields, placeholder_rows):
+        placeholder_rows_sql = (", ".join(row) for row in placeholder_rows)
+        values_sql = ", ".join([f"({sql})" for sql in placeholder_rows_sql])
+        return f"VALUES {values_sql}"
