b399fb66d0f3b673fc7cd84805dc9693659d4d65
MirzoFirdavs <mirzofirdavs@github.com>
2025-04-28 14:44:16 +0300

    fix linters

--- examples/simplecrud/simplecrud/urls.py
+++ examples/simplecrud/simplecrud/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
-    path('create/', views.item_create, name='item_create'),
-    path('update/<int:pk>/', views.item_update, name='item_update'),
-    path('delete/<int:pk>/', views.item_delete, name='item_delete'),
+    path("", views.item_list, name="item_list"),
+    path("create/", views.item_create, name="item_create"),
+    path("update/<int:pk>/", views.item_update, name="item_update"),
+    path("delete/<int:pk>/", views.item_delete, name="item_delete"),
 ]
--- examples/simplecrud/simplecrud/wsgi.py
+++ examples/simplecrud/simplecrud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplecrud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplecrud.settings")
 
 application = get_wsgi_application()
